e1b99457c56899725157bddb1811927ce3779a58
tanzeelak <tnkhan8042@gmail.com>
2018-03-04 07:05:49 -0800

    sending images fosho

--- app/src/main/java/com/example/tanzeelakhan/virgil/MainActivity.java
+++ app/src/main/java/com/example/tanzeelakhan/virgil/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tanzeelakhan.virgil;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,9 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -39,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     public static final MediaType MEDIA_TYPE =
             MediaType.parse("image/jpeg");
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,34 +96,49 @@ public class MainActivity extends AppCompatActivity {
         return temp;
     }
 
+    private void playMp3(byte[] mp3SoundByteArray) {
+        try {
+            // create temp file that will hold byte array
+            File tempMp3 = File.createTempFile("kurchina", "mp3", getCacheDir());
+            tempMp3.deleteOnExit();
+            FileOutputStream fos = new FileOutputStream(tempMp3);
+            fos.write(mp3SoundByteArray);
+            fos.close();
+
+            // resetting mediaplayer instance to evade problems
+            mediaPlayer.reset();
+
+            // In case you run into issues with threading consider new instance like:
+            // MediaPlayer mediaPlayer = new MediaPlayer();
+
+            // Tried passing path directly, but kept getting
+            // "Prepare failed.: status=0x1"
+            // so using file descriptor instead
+            FileInputStream fis = new FileInputStream(tempMp3);
+            mediaPlayer.setDataSource(fis.getFD());
+
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException ex) {
+            String s = ex.toString();
+            ex.printStackTrace();
+        }
+    }
+
 
     protected String sendPost(Bitmap bitmap) {
 
         String bitmapString = BitMapToString(bitmap);
 
         final OkHttpClient client = new OkHttpClient();
-//        JSONObject postdata = new JSONObject();
-//        try {
-//            postdata.put("UserName", "Abhay Anand");
-//            postdata.put("Email", "anand.abhay1910@gmail.com");
-//        } catch(JSONException e){
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-
-//        RequestBody body = RequestBody.create(MEDIA_TYPE,
-//                postdata.toString());
-
-//        RequestBody body = RequestBody.cre
         RequestBody body = RequestBody.create(MEDIA_TYPE, bitmapString);
 
 
         final Request request = new Request.Builder()
                 .url("http://ec2-18-216-113-91.us-east-2.compute.amazonaws.com/api/v1/image\n")
                 .post(body)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Your Token")
-                .addHeader("cache-control", "no-cache")
+                .addHeader("Content-Disposition", "attachment")
+                .addHeader("Content-Type", "image/jpeg")
                 .build();
 
 
@@ -136,8 +157,12 @@ public class MainActivity extends AppCompatActivity {
                 String mMessage = response.body().string();
                 if (response.isSuccessful()) {
                     try {
-                        JSONObject json = new JSONObject(mMessage);
-                        final String serverResponse = json.getString("Your Index");
+//                        JSONObject json = new JSONObject(mMessage);
+//                        final String serverResponse = json.getString("Your Index");
+                        String serverResponse = mMessage;
+                        Log.d("serverResponse", serverResponse);
+                        byte[] audioArray = serverResponse.getBytes();
+//                        playMp3(audioArray);
 
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -149,179 +174,5 @@ public class MainActivity extends AppCompatActivity {
 
         return "0";
     }
-    protected String doInBackground() {
 
-        //works with test case
-        //songText = "All of me";
-        //artistText = "John Legend";
-
-//        String song = songText.toString();
-//        song = song.replaceAll("\\s","+");
-//        String artist = artistText.toString();
-//        artist = artist.replaceAll("\\s","+");
-        // Do some validation here
-
-        try {
-
-            URL url = new URL("http://ec2-18-216-113-91.us-east-2.compute.amazonaws.com/audio");
-//            Log.d("url", url.toString());
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                }
-                bufferedReader.close();
-                Log.d("json object", stringBuilder.toString());
-
-                JSONObject jObject = new JSONObject(stringBuilder.toString());
-                try {
-                    JSONArray jArray = jObject.getJSONArray("search");
-
-                    for (int i = 0; i < jArray.length(); i++) {
-                        try {
-                            JSONObject oneObject = jArray.getJSONObject(i);
-                            // Pulling items from the array
-//                            bpm = oneObject.getString("tempo");
-//                            if (bpm.equals("null")) {
-//                                bpm = "0";
-//                            }
-//                            Log.d("BPM", bpm);
-
-                        } catch (JSONException e) {
-                            // Oops
-                        }
-                    }
-                }
-                catch (Exception e){
-                    Log.d("hello","lol");
-//                    bpm = "0";
-//                    return bpm;
-                    return "0";
-                }
-//                return bpm;
-                return "0";
-            }
-            finally{
-                urlConnection.disconnect();
-            }
-        }
-        catch(Exception e) {
-            Log.d("plso","helloo");
-            Log.e("ERROR", e.getMessage(), e);
-//            bpm = "0";
-//            return bpm;
-            return "0";
-        }
-    }
-    public void sendPost() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Log.d("haha","hahaha");
-                    URL url = new URL("http://ec2-18-216-113-91.us-east-2.compute.amazonaws.com/api/v1/audio");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept","application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-
-//                    JSONObject jsonParam = new JSONObject();
-////                    jsonParam.put("timestamp", 1488873360);
-////                    jsonParam.put("uname", message.getUser());
-////                    jsonParam.put("message", message.getMessage());
-//                    jsonParam.put("latitude", 0D);
-//                    jsonParam.put("longitude", 0D);
-
-//                    Log.i("JSON", jsonParam.toString());
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-//                    os.writeBytes(jsonParam.toString());
-
-                    os.flush();
-                    os.close();
-
-                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-                    Log.i("MSG" , conn.getResponseMessage());
-
-                    conn.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
-    }
-
-//    protected String doInBackground(String songText, String artistText) {
-//
-//        //works with test case
-//        //songText = "All of me";
-//        //artistText = "John Legend";
-//
-//        String song = songText.toString();
-//        song = song.replaceAll("\\s","+");
-//        String artist = artistText.toString();
-//        artist = artist.replaceAll("\\s","+");
-//        // Do some validation here
-//
-//        try {
-//
-////            URL url = new URL(API_URL + "api_key=" + API_KEY + "&type=both&lookup=song:" + song + "%20artist:" + artist);
-//            URL url = new URL("http://ec2-18-216-113-91.us-east-2.compute.amazonaws.com/api/v1/image");
-////            Log.d("url", url.toString());
-//            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-//            try {
-//
-//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-//                StringBuilder stringBuilder = new StringBuilder();
-//                String line;
-//                while ((line = bufferedReader.readLine()) != null) {
-//                    stringBuilder.append(line).append("\n");
-//                }
-//                bufferedReader.close();
-//                Log.d("json object", stringBuilder.toString());
-//
-//                JSONObject jObject = new JSONObject(stringBuilder.toString());
-//                try {
-//                    JSONArray jArray = jObject.getJSONArray("search");
-//
-//                    for (int i = 0; i < jArray.length(); i++) {
-//                        try {
-//                            JSONObject oneObject = jArray.getJSONObject(i);
-//                            // Pulling items from the array
-//                            bpm = oneObject.getString("tempo");
-//                            if (bpm.equals("null")) {
-//                                bpm = "0";
-//                            }
-//                            Log.d("BPM", bpm);
-//
-//                        } catch (JSONException e) {
-//                            // Oops
-//                        }
-//                    }
-//                }
-//                catch (Exception e){
-//                    Log.d("hello","lol");
-//                    bpm = "0";
-//                    return bpm;
-//                }
-//                return bpm;
-//            }
-//            finally{
-//                urlConnection.disconnect();
-//            }
-//        }
-//        catch(Exception e) {
-//            Log.d("plso","helloo");
-//            Log.e("ERROR", e.getMessage(), e);
-//            bpm = "0";
-//            return bpm;
-//        }
-//    }
 }
